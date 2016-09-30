585e0f61050f4fa73c7196d4c3aa5925c52ed99d
jvomarques <jvo.marques@gmail.com>
2016-09-29 23:06:13 -0300

    Atualizando versão fase 1

--- ProjetoDeSoftware/Classe/Extrator.cs
+++ ProjetoDeSoftware/Classe/Extrator.cs
@@ -9,150 +9,97 @@ using System.Threading.Tasks;
 
 namespace ProjetoDeSoftware.Classe
 {
+    //CLASSE COM A FUNÇÃO DE REALIZAR A EXTRAÇÃO DOS DADOS DA API
     class Extrator
     {
-        /// <summary>
-        /// Nível Territorial - Código.
-        /// </summary>
-        public string NC { get; set; }
+        //IMPLEMENTAÇÃO DO PADRÃO SINGLETON
+        private static Extrator instance;
 
-        /// <summary>
-        /// Nível Territorial - Nome.
-        /// </summary>
-        public string NN { get; set; }
+        private Extrator() { }
 
-        /// <summary>
-        /// Dimensão 1 - Código.
-        /// </summary>
-        public string D1C { get; set; }
+        public static Extrator Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Extrator();
+                }
+                return instance;
+            }
+        }
 
-        /// <summary>
-        /// Dimensão 1 - Nome.
-        /// </summary>
-        public string D1N { get; set; }
-
-        /// <summary>
-        /// Dimensão 2 - Código.
-        /// </summary>
-        public string D2C { get; set; }
-
-        /// <summary>
-        /// Dimensão 2 - Nome.
-        /// </summary>
-        public string D2N { get; set; }
-
-        /// <summary>
-        /// Dimensão 3 - Código.
-        /// </summary>
-        public string D3C { get; set; }
-
-        /// <summary>
-        /// Dimensão 3 - Nome.
-        /// </summary>
-        public string D3N { get; set; }
-
-        /// <summary>
-        /// Dimensão 4 - Código.
-        /// </summary>
-        public string D4C { get; set; }
-
-        /// <summary>
-        /// Dimensão 4 - Nome.
-        /// </summary>
-        public string D4N { get; set; }
-
-        /// <summary>
-        /// Dimensão 5 - Código.
-        /// </summary>
-        public string D5C { get; set; }
-
-        /// <summary>
-        /// Dimensão 5 - Nome.
-        /// </summary>
-        public string D5N { get; set; }
-
-        /// <summary>
-        /// Dimensão 6 - Código.
-        /// </summary>
-        public string D6C { get; set; }
-
-        /// <summary>
-        /// Dimensão 6 - Nome.
-        /// </summary>
-        public string D6N { get; set; }
-
-        /// <summary>
-        /// Dimensão 7 - Código.
-        /// </summary>
-        public string D7C { get; set; }
-
-        /// <summary>
-        /// Dimensão 7 - Nome.
-        /// </summary>
-        public string D7N { get; set; }
-
-        /// <summary>
-        /// Dimensão 8 - Código.
-        /// </summary>
-        public string D8C { get; set; }
-
-        /// <summary>
-        /// Dimensão 8 - Nome.
-        /// </summary>
-        public string D8N { get; set; }
-
-        /// <summary>
-        /// Dimensão 9 - Código.
-        /// </summary>
-        public string D9C { get; set; }
-
-        /// <summary>
-        /// Dimensão 9 - Nome.
-        /// </summary>
-        public string D9N { get; set; }
-
-        /// <summary>
-        /// Unidade de medida - Código.
-        /// </summary>
-        public string MC { get; set; }
-
-        /// <summary>
-        /// Unidade de medida - Nome.
-        /// </summary>
-        public string MN { get; set; }
-
-        /// <summary>
-        /// Valor.
-        /// </summary>
+        //VARIAVEL DE RETORNO DA CONSULTA NA API
         public string V { get; set; }
 
-        public List<Extrator> extrairDados() {
+        public static Bairro extrairBairro(string codigo)
+        {
+            Bairro bairro = new Bairro();
+            HttpResponseMessage reposta;
 
-            List<Extrator> dados = new List<Extrator>();
-
-            HttpClient client = new HttpClient();
+            HttpClient cliente = new HttpClient();
             string baseApiAddress = "http://www.sidra.ibge.gov.br";
-            client.BaseAddress = new Uri(baseApiAddress);
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            //HttpResponseMessage response = client.GetAsync("/api/values/t/686/n1/all/v/all/p/last/c318/all/f/u").Result;
-            ///t/3168//n102/2408102019/v/all/p/last/c63/all/f/u
-            //"/t/3213/n102/2408102017/v/all/p/last/c287/all/f/u
-            //Console.WriteLine(valor.D1C + " - " + valor.D1N + " - " + valor.D2N + " - " + valor.D3N + " - " + valor.D4N + " - " + valor.MN + " - " + valor.V);
-            HttpResponseMessage response = client.GetAsync("/api/values/t/3213/n102/2408102017/v/all/p/last/c287/all/f/u").Result;
-            if (response.IsSuccessStatusCode)
-            {
-                IEnumerable<Extrator> valores = response.Content.ReadAsAsync<IEnumerable<Extrator>>().Result;
-                foreach (Extrator valor in valores)
-                {
-                    dados.Add(valor);
-                }
-            }
-            else
-            {
+            cliente.BaseAddress = new Uri(baseApiAddress);
+            cliente.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
+            //POPULAÇÃO TOTAL POR BAIRRO
+            reposta = cliente.GetAsync("/api/values/t/202/n102/" + codigo + "/v/93/p/last/c2/4/f/u").Result;
+            if (reposta.IsSuccessStatusCode)
+            {
+                List<Extrator> valores = reposta.Content.ReadAsAsync<List<Extrator>>().Result;
+                
+                bairro.setPopulacaoH(valores[1].V);
             }
 
-            return dados;
+            reposta = cliente.GetAsync("/api/values/t/202/n102/" + codigo + "/v/93/p/last/c2/5/f/u").Result;
+            if (reposta.IsSuccessStatusCode)
+            {
+                List<Extrator> valores = reposta.Content.ReadAsAsync<List<Extrator>>().Result;
+
+                bairro.setPopulacaoM(valores[1].V);
+            }
+
+            //NUMERO DE ALFABETIZADOS POR BAIRRO
+            reposta = cliente.GetAsync("/api/values/t/1699/n102/" + codigo + "/v/1645/p/last/c2/4/f/u").Result;
+            if (reposta.IsSuccessStatusCode)
+            {
+                List<Extrator> valores = reposta.Content.ReadAsAsync<List<Extrator>>().Result;
+                bairro.setAlfabetizadosH(valores[1].V);
+            }
+            reposta = cliente.GetAsync("/api/values/t/1699/n102/" + codigo + "/v/1645/p/last/c2/5/f/u").Result;
+            if (reposta.IsSuccessStatusCode)
+            {
+                List<Extrator> valores = reposta.Content.ReadAsAsync<List<Extrator>>().Result;
+                bairro.setAlfabetizadosM(valores[1].V);
+            }
+
+            //RENDA MEDIA MENSAL
+            reposta = cliente.GetAsync("/api/values/t/3170/n102/" + codigo + "/v/842/p/last/c2/4/f/u").Result;
+            if (reposta.IsSuccessStatusCode)
+            {
+                List<Extrator> valores = reposta.Content.ReadAsAsync<List<Extrator>>().Result;
+                bairro.setRendaMediaH(valores[1].V);
+            }
+
+            reposta = cliente.GetAsync("/api/values/t/3170/n102/" + codigo + "/v/842/p/last/c2/5/f/u").Result;
+            if (reposta.IsSuccessStatusCode)
+            {
+                List<Extrator> valores = reposta.Content.ReadAsAsync<List<Extrator>>().Result;
+                bairro.setRendaMediaM(valores[1].V);
+            }
+
+            //NUMERO TOTAL DE ALFABETIZADOS 
+            reposta = cliente.GetAsync("/api/values/t/1699/n102/" + codigo + "/v/1645/p/last/c2/6794/f/u").Result;
+            if (reposta.IsSuccessStatusCode)
+            {
+                List<Extrator> valores = reposta.Content.ReadAsAsync<List<Extrator>>().Result;
+                bairro.setAlfabetizadosTotal(valores[1].V);
+
+                int nao_alfa = Convert.ToInt32(bairro.getPopulacaoTotal()) - Convert.ToInt32(bairro.getAlfabetizadosTotal());
+                bairro.setNaoAlfabetizadosTotal(nao_alfa.ToString());
+            }
+
+            return bairro;
         }
     }
 }
--- ProjetoDeSoftware/MainWindow.xaml.cs
+++ ProjetoDeSoftware/MainWindow.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Xceed.Wpf.Toolkit;
+using System.Windows.Controls.DataVisualization.Charting;
+
 
 namespace ProjetoDeSoftware
 {
@@ -19,11 +22,97 @@ namespace ProjetoDeSoftware
     /// </summary>
     public partial class MainWindow : Window
     {
+        
+        //Bairro selecionado no combobox
+        Classe.Territorio bairro_selecionado = new Bairro();
+
         public MainWindow()
         {
             InitializeComponent();
-            Classe.Extrator dados = new Classe.Extrator();
-            datagrid_dados.ItemsSource = dados.extrairDados();
+            cb_bairros.ItemsSource = bairro_selecionado.getNomes();
+            grid_graficos.Visibility = Visibility.Hidden;
         }
+
+        private void bt_pesquisar_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string nome_selecionado = cb_bairros.SelectedItem.ToString();
+                
+                bairro_selecionado = bairro_selecionado.getByNome(nome_selecionado);
+
+                double pop, alf, ren;
+
+                if (rd_sexo.IsChecked == true)
+                {
+                    Classe.Grafico g_populacao = new Classe.Grafico();
+                    g_populacao.setDados("Homens", bairro_selecionado.getPopulacaoH());
+                    g_populacao.setDados("Mulheres", bairro_selecionado.getPopulacaoM());
+                    grafico_pop.DataContext = g_populacao.getGrafico();
+
+                    Classe.Grafico g_alf = new Classe.Grafico();
+                    g_alf.setDados("Homens", bairro_selecionado.getAlfabetizadosH());
+                    g_alf.setDados("Mulheres", bairro_selecionado.getAlfabetizadosM());
+                    grafico_alf.DataContext = g_alf.getGrafico();
+
+                    Classe.Grafico g_renda = new Classe.Grafico();
+                    g_renda.setDados("Homens", bairro_selecionado.getRendaMediaH());
+                    g_renda.setDados("Mulheres", bairro_selecionado.getRendaMediaM());
+                    grafico_renda.DataContext = g_renda.getGrafico();
+
+                    lb_grafico1.Content = "População total";
+                    lb_grafico2.Content = "Número de alfabetizados";
+                    lb_grafico3.Content = "Renda média mensal";
+
+                    grid_graficos.Visibility = Visibility.Visible;
+
+                }
+                else if (rd_alf.IsChecked == true)
+                {
+                    Classe.Grafico g1 = new Classe.Grafico();
+                    g1.setDados("Alfabetizados", bairro_selecionado.getAlfabetizadosTotal());
+                    g1.setDados("Não Alfabetizados", bairro_selecionado.getNaoAlfabetizadosTotal());
+                    grafico_pop.DataContext = g1.getGrafico();
+
+                    lb_grafico1.Content = "Alfabetizados x Não alfabetizados";
+
+                    Classe.Grafico g_alf = new Classe.Grafico();
+                    g_alf.setDados("Homens", bairro_selecionado.getAlfabetizadosH());
+                    g_alf.setDados("Mulheres", bairro_selecionado.getAlfabetizadosM());
+                    grafico_alf.DataContext = g_alf.getGrafico();
+                    lb_grafico2.Content = "Homens alfabetizados x Mulheres alfabetizadas";
+
+                    lb_grafico3.Visibility = Visibility.Hidden;
+                    grafico_renda.Visibility = Visibility.Hidden;
+
+
+                    //Numero de alfabetizados x Nao alfabetizados
+                    //Renda dos alfabetizados x Nao alfabetizados
+                    //Homem alfabetizado x Mulher alfabetizado
+
+                    grid_graficos.Visibility = Visibility.Visible;
+
+
+                }
+
+
+            }
+              catch 
+              {
+                System.Windows.MessageBox.Show("Preencha/Marque todos os campos!");
+              }
+        }
+
+        private void rd_sexo_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rd_alf_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
     }
 }
